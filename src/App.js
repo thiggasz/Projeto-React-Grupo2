cb280355f8660158b3b10599364734814664e25b
thiggasz <thiagozduarte@gmail.com>
2022-09-18 13:58:42 -0300

    Setting routes.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Home from './pages/home'
+import Register from './pages/register';
+import Management from './pages/management';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="cadastro" element={<Register />}/>
+        <Route path="/gerenciamento" element={<Management/>}/>    
+      </Routes></BrowserRouter>
   );
 }
 
