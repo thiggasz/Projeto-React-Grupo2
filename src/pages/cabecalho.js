4e305e1506c7b4f1541f16ee28c5cd71f516392d
thiggasz <thiagozduarte@gmail.com>
2022-09-20 19:01:25 -0300

    Logo direcionar para a página inicial

--- src/pages/cabecalho.js
+++ src/pages/cabecalho.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React from 'react' 
+import ImgCab from './patinha.png'
 
 export default function Cabecalho(){
     return( 
         <React.Fragment>
-            <div className='topo'>meupet</div>
+            <div className='topo'>
+                <a href="./">
+                <span className='topopre' >meu</span>pet
+                <img src={ImgCab} className='imgcab' alt="Pata de cachorro"></img></a>
+            </div>
         </React.Fragment>
     )
 }
