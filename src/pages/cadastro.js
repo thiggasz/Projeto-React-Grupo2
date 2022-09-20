70fca92a631845ae5c323574325dbc1aa3d7bc47
thiggasz <thiagozduarte@gmail.com>
2022-09-20 16:33:45 -0300

    Página de cadastro

--- src/pages/cadastro.js
+++ src/pages/cadastro.js
@@ -1,13 +1,62 @@
-import React from 'react';
+import React from 'react'
+import ImgCad from './cad.jpg'
 import EstCad from './styles/cadastro.css'
 
 export default function Register(){
     return(
         <React.Fragment>
-            <div className='titulocad' >O que deseja cadastrar?</div>
-            <div className='botaocad1'><button className='botao' type='button'>Espécies</button></div>
-            <div className='botaocad2'><button className='botao' type='button'>Animais</button></div>
-            <input type='text' name='Nome' className='caixas'></input>
+            <form className='formulario'>
+            <div> 
+                <div className='cabform'>Dados do animal</div>
+                <div className='alinhamentocad'>
+                    <label for='Nome'>Nome</label>
+                    <input type='text' name='Nome' id='Nome' className='caixas'></input>
+                </div>
+                <div className='alinhamentocad'>
+                    <label for='Nome'>Idade</label>
+                    <input type='number' name='Idade' id='Idade' className='caixas'></input>
+                </div>
+                <div className='alinhamentocad'>
+                    <label for='Nome'>Peso</label>
+                    <input type='number' name='Peso' id='Peso' className='caixas'></input>
+                </div>
+                <div  className='alinhamentocad'> 
+                    <label for='Nome'>Status do paciente</label>
+                    <input type='text' name='Status' id='Status' className='caixas'></input>
+                </div>
+                <div  className='alinhamentocad'>
+                    <label for='Nome'>Custo do tratamento</label>
+                    <input type='number' name='Custo' id='Custo' className='caixas'></input>
+                </div>
+            </div>
+            <div>
+                <div className='cabform'>Dados do dono</div>
+                <div  className='alinhamentocad'>
+                    <label for='Nome'>Custo do tratamento</label>
+                    <input type='number' name='Custo' id='Custo' className='caixas'></input>
+                </div>
+                <div  className='alinhamentocad'>
+                    <label for='Nome'>Custo do tratamento</label>
+                    <input type='number' name='Custo' id='Custo' className='caixas'></input>
+                </div>
+                <div  className='alinhamentocad'>
+                    <label for='Nome'>Custo do tratamento</label>
+                    <input type='number' name='Custo' id='Custo' className='caixas'></input>
+                </div>
+                <div  className='alinhamentocad'>
+                    <label for='Nome'>Custo do tratamento</label>
+                    <input type='number' name='Custo' id='Custo' className='caixas'></input>
+                </div>
+                <div  className='alinhamentocad'>
+                    <label for='Nome'>Custo do tratamento</label>
+                    <input type='number' name='Custo' id='Custo' className='caixas'></input>
+                </div>
+            </div>
+                <div className='botaocad'>
+                    <a href="./gerencamento"><button className='botao' type='button'>Gerenciamento</button></a>
+                </div>
+            </form>
+            <img src={ImgCad} className='img' height={566} alt="Cachorro"></img>
         </React.Fragment>
     )
 }
