29c075bfc0898748f3ee82322cce2fdfa5b15244
thiggasz <thiagozduarte@gmail.com>
2022-09-22 19:36:19 -0300

    Formulário

--- src/pages/cadastro.js
+++ src/pages/cadastro.js
@@ -5,50 +5,48 @@ import EstCad from './styles/cadastro.css'
 export default function Register(){
     return(
         <React.Fragment>
-            <div className='cabform'>Dados do animal</div>
+            <div className='cabform'>Dados do cliente</div>
             <form className='formulario'>
             <div> 
                 <div className='alinhamentocad'>
-                    <label for='Nome'>Nome:</label>
-                    <input type='text' name='Nome' id='Nome' className='caixas'></input>
+                    <label for='Nome'>Nome do paciente:</label>
+                    <input type='text' name='Nome' id='Nome' className='caixas' required></input>
                 </div>
                 <div className='alinhamentocad'>
-                    <label for='Nome'>Idade:</label>
-                    <input type='number' name='Idade' id='Idade' className='caixas'></input>
+                    <label for='Idade'>Idade:</label>
+                    <input type='number' name='Idade' id='Idade' className='caixas' required></input>
                 </div>
                 <div className='alinhamentocad'>
-                    <label for='Nome'>Peso:</label>
-                    <input type='number' name='Peso' id='Peso' className='caixas'></input>
+                    <label for='Peso'>Peso:</label>
+                    <input type='number' name='Peso' id='Peso' className='caixas' required></input>
                 </div>
                 <div  className='alinhamentocad'> 
-                    <label for='Nome'>Status do paciente:</label>
-                    <input type='text' name='Status' id='Status' className='caixas'></input>
+                    <label for='Status'>Status do paciente:</label>
+                    <input type='text' name='Status' id='Status' className='caixas' required></input>
                 </div>
                 <div  className='alinhamentocad'>
-                    <label for='Nome'>Custo do tratamento:</label>
-                    <input type='number' name='Custo' id='Custo' className='caixas'></input>
+                    <label for='Custo'>Custo do tratamento:</label>
+                    <input type='number' name='Custo' id='Custo' className='caixas' required></input>
                 </div>
             </div>
-            </form>
-            <div className='cabform'>Dados do dono</div>
-            <form className='formulario'>
             <div>
                 <div  className='alinhamentocad'>
-                    <label for='Nome'>Nome:</label>
-                    <input type='number' name='Custo' id='Custo' className='caixas'></input>
+                    <label for='NomeCli'>Nome do dono:</label>
+                    <input type='text' name='Custo' id='Nome' className='caixas' required></input>
                 </div>
                 <div  className='alinhamentocad'>
-                    <label for='Nome'>Email:</label>
-                    <input type='number' name='Custo' id='Custo' className='caixas'></input>
+                    <label for='Email'>Email:</label>
+                    <input type='email' name='Custo' id='Email' className='caixas' required></input>
                 </div>
                 <div  className='alinhamentocad'>
-                    <label for='Nome'>Endereço:</label>
-                    <input type='number' name='Custo' id='Custo' className='caixas'></input>
+                    <label for='Endereço'>Endereço:</label>
+                    <input type='text' name='Custo' id='Endereço' className='caixas' required></input>
                 </div>
                 <div  className='alinhamentocad'>
-                    <label for='Nome'>Telefone:</label>
-                    <input type='number' name='Custo' id='Custo' className='caixas'></input>
+                    <label for='TEL'>Telefone de contato:</label>
+                    <input type='tel' name='Custo' id='Telefone' className='caixas' required></input>
                 </div>
+                <input type='submit' name='Envio' className='envio'></input>
             </div>
             </form>
             <img src={ImgCad} className='img' height={700} alt="Cachorro"></img>
